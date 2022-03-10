68097c133b6f4e5de4f13c6982bb780b8e3342c8
Nguyễn Trung Hiếu <hieu21nt@gmail.com>
2022-03-10 19:29:17 +0700

    Update apiConfig.js

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
   baseUrl: 'https://api.themoviedb.org/3/',
-  apiKey: '25650357fdb5a7fd7fe59ccc6939fd60',
+  apiKey: 'get from themoviedb.org',
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 }
